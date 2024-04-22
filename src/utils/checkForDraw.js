a4466c054f2e48adb2d855b901c90f4d15672e25
Ketan Chhajed <ketanchhajed07@gmail.com>
2024-04-22 19:35:19 +0530

    check draw function fixed

--- src/utils/checkForDraw.js
+++ src/utils/checkForDraw.js
@@ -2,7 +2,7 @@ function checkForDraw(board) {
   // No win, check for draw
   for (let row = 0; row < board.length; row++) {
     for (let col = 0; col < board[row].length; col++) {
-      if (board[col][row] === null) {
+      if (board[row][col] === null) {
         return false; // If any cell is empty, it's not a draw, return no win
       }
     }
